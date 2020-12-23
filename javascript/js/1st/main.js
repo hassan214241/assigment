c9bd3b3c739a9b9ab2aaefe1d00a9125a96506f0
Muhammad Hassan <hassan214241@gmail.com>
2020-12-23 23:37:15 +0500

    Grade show

---
+++ javascript/js/1st/main.js
@@ -0,0 +1,23 @@
+//const d = 20;
+//console.log(d);
+
+//let a = 3;
+//let b = 5;
+//let c = a + b;
+//console.log(a+b);
+
+// let firstname ="Muhammad";
+// let lastname = " Hassan";
+// console.log(firstname +""+lastname);
+
+const marks = 20;
+
+if (marks >= 80){
+    console.log("Grade is A");
+}else if (marks >=70){
+    console.log("Grade is B");
+}else if (marks >=60){
+    console.log("Grade is c");
+}else {
+    console.log("failed");
+}
